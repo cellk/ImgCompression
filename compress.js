f9a9bc3033b4980c78d0f4df610620597ddf09df
Kenny Tchu <kenny_tchu@yahoo.fr>
2017-11-27 16:31:58 -0500

    Sending compressed image as a data form

--- compress.js
+++ compress.js
@@ -9,20 +9,7 @@
  * 
  */
 
-//base64js
-(function(r){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=r()}else if(typeof define==="function"&&define.amd){define([],r)}else{var e;if(typeof window!=="undefined"){e=window}else if(typeof global!=="undefined"){e=global}else if(typeof self!=="undefined"){e=self}else{e=this}e.base64js=r()}})(function(){var r,e,t;return function r(e,t,n){function o(i,a){if(!t[i]){if(!e[i]){var u=typeof require=="function"&&require;if(!a&&u)return u(i,!0);if(f)return f(i,!0);var d=new Error("Cannot find module '"+i+"'");throw d.code="MODULE_NOT_FOUND",d}var c=t[i]={exports:{}};e[i][0].call(c.exports,function(r){var t=e[i][1][r];return o(t?t:r)},c,c.exports,r,e,t,n)}return t[i].exports}var f=typeof require=="function"&&require;for(var i=0;i<n.length;i++)o(n[i]);return o}({"/":[function(r,e,t){"use strict";t.byteLength=c;t.toByteArray=v;t.fromByteArray=s;var n=[];var o=[];var f=typeof Uint8Array!=="undefined"?Uint8Array:Array;var i="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";for(var a=0,u=i.length;a<u;++a){n[a]=i[a];o[i.charCodeAt(a)]=a}o["-".charCodeAt(0)]=62;o["_".charCodeAt(0)]=63;function d(r){var e=r.length;if(e%4>0){throw new Error("Invalid string. Length must be a multiple of 4")}return r[e-2]==="="?2:r[e-1]==="="?1:0}function c(r){return r.length*3/4-d(r)}function v(r){var e,t,n,i,a;var u=r.length;i=d(r);a=new f(u*3/4-i);t=i>0?u-4:u;var c=0;for(e=0;e<t;e+=4){n=o[r.charCodeAt(e)]<<18|o[r.charCodeAt(e+1)]<<12|o[r.charCodeAt(e+2)]<<6|o[r.charCodeAt(e+3)];a[c++]=n>>16&255;a[c++]=n>>8&255;a[c++]=n&255}if(i===2){n=o[r.charCodeAt(e)]<<2|o[r.charCodeAt(e+1)]>>4;a[c++]=n&255}else if(i===1){n=o[r.charCodeAt(e)]<<10|o[r.charCodeAt(e+1)]<<4|o[r.charCodeAt(e+2)]>>2;a[c++]=n>>8&255;a[c++]=n&255}return a}function l(r){return n[r>>18&63]+n[r>>12&63]+n[r>>6&63]+n[r&63]}function h(r,e,t){var n;var o=[];for(var f=e;f<t;f+=3){n=(r[f]<<16)+(r[f+1]<<8)+r[f+2];o.push(l(n))}return o.join("")}function s(r){var e;var t=r.length;var o=t%3;var f="";var i=[];var a=16383;for(var u=0,d=t-o;u<d;u+=a){i.push(h(r,u,u+a>d?d:u+a))}if(o===1){e=r[t-1];f+=n[e>>2];f+=n[e<<4&63];f+="=="}else if(o===2){e=(r[t-2]<<8)+r[t-1];f+=n[e>>10];f+=n[e>>4&63];f+=n[e<<2&63];f+="="}i.push(f);return i.join("")}},{}]},{},[])("/")});
-
-//TextEncoderLite 
-function TextEncoderLite(){}function TextDecoderLite(){}(function(){'use strict';function utf8ToBytes(a,b){b=b||Infinity;for(var c,d=a.length,e=null,f=[],g=0;g<d;g++){if(c=a.charCodeAt(g),!(55295<c&&57344>c))e&&(-1<(b-=3)&&f.push(239,191,189),e=null);else if(e){if(56320>c){-1<(b-=3)&&f.push(239,191,189),e=c;continue}else c=65536|(e-55296<<10|c-56320),e=null;}else if(56319<c){-1<(b-=3)&&f.push(239,191,189);continue}else if(g+1===d){-1<(b-=3)&&f.push(239,191,189);continue}else{e=c;continue}if(128>c){if(0>(b-=1))break;f.push(c)}else if(2048>c){if(0>(b-=2))break;f.push(192|c>>6,128|63&c)}else if(65536>c){if(0>(b-=3))break;f.push(224|c>>12,128|63&c>>6,128|63&c)}else if(2097152>c){if(0>(b-=4))break;f.push(240|c>>18,128|63&c>>12,128|63&c>>6,128|63&c)}else throw new Error('Invalid code point')}return f}function utf8Slice(a,b,c){var d='',e='';c=Math.min(a.length,c||Infinity),b=b||0;for(var f=b;f<c;f++)127>=a[f]?(d+=decodeUtf8Char(e)+String.fromCharCode(a[f]),e=''):e+='%'+a[f].toString(16);return d+decodeUtf8Char(e)}function decodeUtf8Char(a){try{return decodeURIComponent(a)}catch(b){return String.fromCharCode(65533)}}TextEncoderLite.prototype.encode=function(a){var b;return b='undefined'==typeof Uint8Array?utf8ToBytes(a):new Uint8Array(utf8ToBytes(a)),b},TextDecoderLite.prototype.decode=function(a){return utf8Slice(a,0,a.length)}})();
-
-function base64EncodingUTF8(str) {
-    var encoded = new TextEncoderLite('utf-8').encode(str);        
-    var b64Encoded = base64js.fromByteArray(encoded);
-    return b64Encoded;
-}
-
 /**
- * Create the jic object.
  * @constructor
  */
 
@@ -36,17 +23,17 @@ var kt = {
      * @param {function} (OPTIONAL) duringCallback The callback called to be notified about the image's upload progress.
      * @param {Object} (OPTIONAL) customHeaders An object representing key-value  properties to inject to the request header.
      */
-    compress : function(params, f, successCallback, errorCallback, duringCallback, customHeaders){
-        
-        if(f.files.length == 0){
+    compress: function (params, f, successCallback, errorCallback, duringCallback, customHeaders) {
+
+        if (f.files.length == 0) {
             alert('Not file to compress');
             return;
         }
-        
+
         var fType = f.files[0].type;
         var file = f.files[0];
         var img = new Image();
-        
+
         if (!fType.match('image.*')) {
             alert('Only images allowed for upload')
             return;
@@ -54,7 +41,10 @@ var kt = {
 
         var quality = params.quality;
         var uploadlink = params.uploadLink;
-        
+
+        //console.log(f.files);
+        //return;
+
         if (fType === "image/png") {
             ext = ".png";
         } else if (fType === "image/webp") {
@@ -66,31 +56,43 @@ var kt = {
         } else {
             ext = ".jpg";
         }
-        
+
         var reader = new FileReader();
         reader.onload = function (e) {
+            //console.log(e, reader)
             img.src = reader.result;
+
+            img.onerror = function (e) {
+                alert("File format is not supported");
+            };
+
             //Compression
             img.onload = function (e) {
                 e.stopPropagation();
                 var mime_type;
-                var cvs = document.createElement('canvas');
                 mime_type = fType;
+                var cvs = document.createElement('canvas');
+
+                var dataUri = cvs.toDataURL(mime_type);
+                if (dataUri.indexOf(mime_type) < 0) {
+                    console.log('not supported');
+                }
+
+
 
                 cvs.width = this.naturalWidth;
                 cvs.height = this.naturalHeight;
                 var ctx = cvs.getContext("2d").drawImage(this, 0, 0);
                 var newImageData = cvs.toDataURL(mime_type, quality / 100);
-                
-                //console.log(newImageData);
-                var preview = document.getElementById('preview');
+
+                //var preview = document.getElementById('preview');
                 //preview.src = newImageData;
-                
-                
-                kt.upload(newImageData, uploadlink, 'receiptImage', fType, 'upload' + ext, successCallback, errorCallback, duringCallback, customHeaders);
+
+
+                kt.upload(newImageData, uploadlink, 'receiptImage', successCallback, errorCallback, duringCallback, customHeaders);
             }
         }
-        reader.readAsDataURL(file);    
+        reader.readAsDataURL(file);
     },
     /**
      * Receives an Image Object and upload it to the server via ajax
@@ -104,29 +106,10 @@ var kt = {
      * @param {Object} (OPTIONAL) customHeaders An object representing key-value  properties to inject to the request header.
      */
 
-    upload: function (imgData, upload_url, file_input_name, filetype, filename, successCallback, errorCallback, duringCallback, customHeaders) {
-
-        //ADD sendAsBinary compatibilty to older browsers
-        if (XMLHttpRequest.prototype.sendAsBinary === undefined) {
-            XMLHttpRequest.prototype.sendAsBinary = function (string) {
-                var bytes = Array.prototype.map.call(string, function (c) {
-                    return c.charCodeAt(0) & 0xff;
-                });
-                this.send(new Uint8Array(bytes).buffer);
-            };
-        }
-
-        var type = filetype;
-
-        var data = imgData;
-        //console.log(data);
-        //console.log(base64EncodingUTF8(data))
+    upload: function (imgData, upload_url, file_input_name, successCallback, errorCallback, duringCallback, customHeaders) {
 
         var xhr = new XMLHttpRequest();
         xhr.open('POST', upload_url, true);
-        var boundary = 'KTboundary';
-
-        xhr.setRequestHeader('Content-Type', 'multipart/form-data; boundary=' + boundary);
 
         // Set custom request headers if customHeaders parameter is provided
         if (customHeaders && typeof customHeaders === "object") {
@@ -144,7 +127,9 @@ var kt = {
             };
         }
 
-        xhr.sendAsBinary(['--' + boundary, 'Content-Disposition: form-data; name="' + file_input_name + '"; filename="' + filename + '"', 'Content-Type: ' + type, '', base64EncodingUTF8(data), '--' + boundary + '--'].join('\r\n'));
+        var formData = new FormData();
+        formData.append(file_input_name, imgData);
+        xhr.send(formData);
 
         xhr.onreadystatechange = function () {
             if (this.readyState == 4) {
@@ -157,7 +142,5 @@ var kt = {
                 }
             }
         };
-
-
     }
 };
